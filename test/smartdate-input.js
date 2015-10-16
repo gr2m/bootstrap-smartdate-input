2c827ec3b61ab7c55e77fa7e435ba9810a5096c4
zoepage <o.gasidlo@gmail.com>
2015-10-16 11:22:09 +0200

    play around and have fun

--- test/smartdate-input.js
+++ test/smartdate-input.js
@@ -1,38 +1,38 @@
-var chai        = require('chai')
+/* global describe, before, after, it*/
 var webdriverio = require('webdriverio')
+var chai = require('chai')
 var chaiAsPromised = require('chai-as-promised')
 
-var assert      = chai.assert    
+var assert = chai.assert
 chai.use(chaiAsPromised)
-
-var expect = chai.expect
 chai.Should()
 
-describe('=== smartDate-input ===', function(){
+describe('=== smartDate-input ===', function () {
 
-    this.timeout(90000)
-    var client = {}
+  this.timeout(90000)
+  var client = {}
 
-    before(function (done) {
-            client = webdriverio.remote({ desiredCapabilities: {browserName: 'chrome'} })
-            client.init(done)
-    })
+  before(function (done) {
+    client = webdriverio.remote({ desiredCapabilities: {browserName: 'chrome'} })
+    client.init(done)
+  })
 
-    it('smartDate, set new Date', function(done) {
-      client
-        .url('file://' + __dirname + '/../index.html')
-        .execute("$('.input-lg').smartDate('set', 'Tuesday, October 13, 2015 6:07pm')")
-        .catch(function(err){
-          console.log('🚨 🚨 🚨 🚨 🚨 🚨 ')
-          console.log(err)
-        })
-        .getValue('.input-lg', function (err, val) {
-          assert.strictEqual(val, 'Tuesday, October 13, 2015 6:07pm')
-        })
-        .call(done)
+  it('smartDate, set new Date', function (done) {
+    client
+      .url('file://' + __dirname + '/../index.html')
+      .execute("$('.input-lg').smartDate('set', 'Tuesday, October 13, 2015 6:07pm')")
+      .catch(function (err) {
+        console.log('🚨 🚨 🚨 🚨 🚨 🚨 ')
+        console.log(err)
       })
+      .getValue('.input-lg', function (err, val) {
+        assert.equal(err, undefined)
+        assert.strictEqual(val, 'Tuesday, October 13, 2015 6:07pm')
+      })
+      .call(done)
+  })
 
-    after(function(done) {
-        client.end(done)
-    })
+  after(function (done) {
+    client.end(done)
+  })
 })
